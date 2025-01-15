ec736c36bfc1a365ce78f362a374003adf7903af
Onkarborude77 <onkar.wuwgw22@sinhgad.edu>
2025-01-15 20:51:29 +0530

    Navbar Updated

--- iskon-app/src/App.jsx
+++ iskon-app/src/App.jsx
@@ -2,15 +2,18 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './HomePage/HomePage'
-
+import Navbar from './components/Navbar';
 
 // Component for Handling Routes
 function AnimatedRoutes() {
   const location = useLocation();
   return (
+    <div>
+    <Navbar/>
     <Routes location={location} key={location.pathname}>
         <Route path="/" element={<HomePage />} />
     </Routes>
+    </div>
   );
 }
 
---
+++ iskon-app/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <div className='nav'>
+
+       <img src="https://iskconmumbaipull-21250.kxcdn.com/bi_theme_snippets/static/src/img/logo-black-header.png" alt="logo" width={50} height={50}
+         />
+        <h3>ISKCON</h3>
+      
+        <div className='btn'>
+        <Link id="ele" to="/">Home</Link>
+        <Link id="ele" to="/About">About</Link>
+        <Link id="ele" to="/Contact">Contact</Link>
+        <Link id="ele" to="/ServicesPage">Service</Link>
+        <div className="dropdown">
+            <span id="ele">Login</span>
+           <div className="dropdown-content">
+            <p><Link id='drop-item' to="/AdminLogin">Admin</Link></p>
+            <p><Link id='drop-item' to="/VolenteerLogin">Volenteer</Link></p>
+           </div>
+        </div>
+        <div className="dropdown">
+            <span id="ele">SignUp</span>
+           <div className="dropdown-content">
+            <p><Link id='drop-item' to="/AdminSignUp">Admin</Link></p>
+            <p><Link id='drop-item' to="/VolenteerSignUp">Volenteer</Link></p>
+           </div>
+        </div>
+        
+      
+        </div>
+
+    </div>
+  )
+}
