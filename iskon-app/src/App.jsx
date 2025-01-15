3d075a6a4a0a96098181204612b7551c103cce42
ASH <144093084+ashutoshh-17@users.noreply.github.com>
2025-01-15 13:31:19 +0530

    Created Project Structure

--- iskon-app/src/App.jsx
+++ iskon-app/src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css'
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import HomePage from './HomePage/HomePage'
+
+
+// Component for Handling Routes
+function AnimatedRoutes() {
+  const location = useLocation();
+  return (
+    <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
+}
 
 function App() {
-  
-
   return (
-    <>
-      
-    </>
+    <Router>
+      <AnimatedRoutes/>
+    </Router>
   )
 }
 
